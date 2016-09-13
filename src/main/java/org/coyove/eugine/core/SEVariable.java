5502578238f83f07c140d33b89bd6a4b7fb0b418
coyove <coyove@hotmail.com>
2016-09-14 00:00:19 +0900

    Strict mode

--- src/main/java/org/coyove/eugine/core/SEVariable.java
+++ src/main/java/org/coyove/eugine/core/SEVariable.java
@@ -19,21 +19,20 @@ public class SEVariable extends SExpression {
     @Override
     public SValue evaluate(ExecEnvironment env) throws VMException {
         if (!env.containsKey(varName)) {
-            SValue tmp = (varName.charAt(0) == '@') ? new SString(varName.substring(1), true) : new SNull(true);
+            if (varName.charAt(0) == '@') {
+                SValue tmp = new SString(varName.substring(1), true);
+                env.put(varName, tmp);
+                return tmp;
+            } else {
+                if (env.strict)
+                    throw  new VMException("strict mode", headAtom);
 
-            if (env.containsKey(varName))
-                return env.get(varName);
-
-            env.put(varName, tmp);
-            return tmp;
+                SValue tmp = new SNull(true);
+                env.put(varName, tmp);
+                return tmp;
+            }
         }
 
-        SValue imd = env.get(varName).evaluate(env);
-
-//        if (imd instanceof SClosure && ((SClosure) imd).arguments.size() == 0)
-//            // it is a closure that accepts 0 arguments, so just directly evaluate it
-//            return ((SClosure) imd).body.evaluate(env);
-//        else
-            return imd;
+        return env.get(varName);
     }
 }
