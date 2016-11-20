709a58340d39b11006177dd5449a4eae0129dc3f
coyove <coyove@hotmail.com>
2016-11-20 10:25:03 +0900

    Math operation optimization

--- src/main/java/org/coyove/eugine/core/PMath.java
+++ src/main/java/org/coyove/eugine/core/PMath.java
@@ -18,9 +18,11 @@ public class PMath extends SExpression {
     @ReplaceableVariable
     private SExpression right;
 
+    public Object[] stack = new Object[3];
+
     private ACTION action;
 
-    public enum ACTION {SUBTRACT, MULTIPLY, DIVIDE, MODULAR}
+    public enum ACTION {ADD, SUBTRACT, MULTIPLY, DIVIDE, MODULAR}
 
     public PMath() {}
 
@@ -32,7 +34,7 @@ public class PMath extends SExpression {
         right = args.get(1);
     }
 
-    public static SValue perform(SValue left, SValue right, ACTION action, Atom atom) throws EgException {
+    public SValue perform(SValue left, SValue right) throws EgException {
         switch (action) {
             case SUBTRACT:
                 if (left instanceof SInteger) {
@@ -97,71 +99,58 @@ public class PMath extends SExpression {
         if (this.left instanceof PVariable && this.right instanceof PVariable) {
             if (((PVariable) this.left).varName.equals(((PVariable) this.right).varName)) {
                 SValue left = this.left.evaluate(env);
-                switch (action) {
-                    case SUBTRACT:
-                        if (left instanceof SInteger) {
-                            return new SInteger(0);
-                        } else if (left instanceof SDouble) {
-                            return new SDouble(0.0);
-                        } else {
-                            throw new EgException(7040, "invalid number: " + left, atom);
-                        }
-                    case MULTIPLY:
-                        if (left instanceof SInteger) {
-                            Long v = left.get();
-                            return new SInteger(v * v);
-                        } else if (left instanceof SDouble) {
-                            Double v = left.get();
-                            return new SDouble(v * v);
-                        } else {
-                            throw new EgException(7040, "invalid number: " + left, atom);
-                        }
-                    case DIVIDE:
-                        if (left instanceof SInteger) {
-                            Long lr = left.get();
-                            if (lr == 0) {
-                                throw new EgException(7041, "divided by zero", atom);
-                            } else {
-                                return new SInteger(1);
-                            }
-                        } else if (left instanceof SDouble) {
-                            Double dr = left.get();
-                            if (Math.abs(dr) < 1e-6) {
-                                throw new EgException(7041, "divided by zero", atom);
-                            } else {
-                                return new SDouble(1.0);
-                            }
-                        } else {
-                            throw new EgException(7040, "invalid number: " + left, atom);
-                        }
-                    case MODULAR:
-                        if (left instanceof SInteger) {
-                            Long lr = left.get();
-                            if (lr == 0) {
-                                throw new EgException(7041, "moded by zero", atom);
-                            } else {
-                                return new SInteger(0);
-                            }
-                        } else if (left instanceof SDouble) {
-                            Double dr = left.get();
-                            if (Math.abs(dr) < 1e-6) {
-                                throw new EgException(7041, "moded by zero", atom);
-                            } else {
-                                return new SDouble(0.0);
-                            }
-                        } else {
-                            throw new EgException(7040, "invalid number: " + left, atom);
-                        }
-                    default:
-                        // never happen
-                        return null;
+                if (action == ACTION.MULTIPLY) {
+                    if (stack[0] == null) {
+                        stack[0] = stack[1] = ((PVariable) this.left).varName;
+                        stack[2] = ACTION.MULTIPLY;
+                    }
+
+                    if (left instanceof SInteger) {
+                        Long v = left.get();
+                        return new SInteger(v * v);
+                    } else if (left instanceof SDouble) {
+                        Double v = left.get();
+                        return new SDouble(v * v);
+                    } else {
+                        throw new EgException(7040, "invalid number: " + left, atom);
+                    }
                 }
+
+                // basically you should write something like x - x, x / x or x % x
             }
         }
 
         SValue left = this.left.evaluate(env);
         SValue right = this.right.evaluate(env);
-        return perform(left, right, this.action, atom);
+        SValue ret = perform(left, right);
+
+        if (stack[0] == null) {
+            if (this.left instanceof SInteger) {
+                stack[0] = ((SInteger) this.left).underlying;
+            } else if (this.left instanceof SDouble) {
+                stack[0] = ((SDouble) this.left).underlying;
+            } else {
+                stack[0] = this.left;
+            }
+
+            if (this.right instanceof SInteger) {
+                stack[1] = ((SInteger) this.right).underlying;
+                if (stack[0] instanceof Double) {
+                    stack[1] = ((Long) stack[1]).doubleValue();
+                }
+            } else if (this.right instanceof SDouble) {
+                stack[1] = ((SDouble) this.right).underlying;
+                if (stack[0] instanceof Long) {
+                    stack[1] = ((Double) stack[1]).longValue();
+                }
+            } else {
+                stack[1] = this.right;
+            }
+
+            stack[2] = action;
+        }
+
+        return ret;
     }
 
     @Override
